e611dcaf21aee86e4a8f8011089417a70d142e00
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2025-08-22 12:04:40 +0530

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // looks at your React components
-  ],
-  darkMode: "class", // so your dark/light mode works
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // keeps your dark: classes working
   theme: {
     extend: {},
   },
